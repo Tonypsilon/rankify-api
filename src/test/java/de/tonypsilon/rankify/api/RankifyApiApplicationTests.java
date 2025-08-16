fb889c60fe05d4b4b4785b8f14535239fdee9896
Tonypsilon <tony.schwedek@posteo.de>
2025-08-16 15:55:28 +0200

    Add test containers to the Application test so that it is not dependent on an externally running Postgres DB.

--- src/test/java/de/tonypsilon/rankify/api/RankifyApiApplicationTests.java
+++ src/test/java/de/tonypsilon/rankify/api/RankifyApiApplicationTests.java
@@ -2,12 +2,36 @@ package de.tonypsilon.rankify.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class RankifyApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @SuppressWarnings("resource") // Managed by Testcontainers JUnit extension
+    @Container
+    static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
+            .withDatabaseName("rankify_test")
+            .withUsername("test")
+            .withPassword("test");
+
+    @DynamicPropertySource
+    static void overrideDataSourceProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
+        // Let Liquibase run against container DB (schema creation). If wanting faster startup, could disable.
+        // registry.add("spring.liquibase.enabled", () -> "false");
+    }
+
+    @Test
+    void contextLoads() {
+        // If the application context starts, DB connectivity + migrations worked.
+    }
 
 }
